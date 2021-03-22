5e17aa093541a7da0f026b77c53c8afda65528c1
Ankur Joshi <ankur.joshi@nagarro.com>
2021-03-22 09:09:02 +0530

    Updates

--- src/main/java/com/vaccnow/covidvaccination/controller/AvailabilityController.java
+++ src/main/java/com/vaccnow/covidvaccination/controller/AvailabilityController.java
@@ -1,16 +1,12 @@
 package com.vaccnow.covidvaccination.controller;
 
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +15,6 @@ import com.vaccnow.covidvaccination.constants.Constants;
 import com.vaccnow.covidvaccination.dto.BranchDTO;
 import com.vaccnow.covidvaccination.dto.TimeSlotDTO;
 import com.vaccnow.covidvaccination.dto.VaccineDTO;
-import com.vaccnow.covidvaccination.model.Branch;
-import com.vaccnow.covidvaccination.model.Vaccine;
 import com.vaccnow.covidvaccination.repository.BranchRepository;
 import com.vaccnow.covidvaccination.response.ResponseBody;
 import com.vaccnow.covidvaccination.service.BranchService;
@@ -36,21 +30,8 @@ public class AvailabilityController {
 	@Autowired
 	VaccineService vaccineService;
 
-
 	@Autowired
 	BranchRepository branchRepository;
-
-	@PostMapping("/{name}")
-	public ResponseEntity<Branch> saveBranch(@PathVariable String name) {
-		Branch branch = new Branch();
-		branch.setName(name);
-		Vaccine vaccine = new Vaccine();
-		vaccine.setName("ABC");
-		branch.setVaccines(Arrays.asList(vaccine));
-		branch.setTimeFrom(LocalTime.parse("09:00:00", DateTimeFormatter.ISO_TIME));
-		branch.setTimeTo(LocalTime.parse("18:00:00", DateTimeFormatter.ISO_TIME));
-		return ResponseEntity.ok(branchRepository.save(branch));
-	}
 
 	@GetMapping
 	public ResponseEntity<ResponseBody<BranchDTO>> getAllBranches(
